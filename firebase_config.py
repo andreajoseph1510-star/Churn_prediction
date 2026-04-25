1d47f5689c82ec8c83ae7ab81773f96e117a1462
andreajoseph1510-star <andreajoseph1510@gmail.com>
2026-04-25 14:41:48 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,17 @@
+import os
 import pyrebase
 
 firebaseConfig = {
-    "apiKey": "AIzaSyBbs51VbRDgmxWrxdycRQTMXCx4tTRL6qo",
-    "authDomain": "churn-prediction-with-ai.firebaseapp.com",
-    "projectId": "churn-prediction-with-ai",
-    "storageBucket": "churn-prediction-with-ai.firebasestorage.app",
-    "messagingSenderId": "579432513955",
-    "appId": "1:579432513955:web:c68cfd8f4cf2f0b7c05a16",
-    "databaseURL":"https://console.firebase.google.com/u/0/project/churn-prediction-with-ai/database/churn-prediction-with-ai-default-rtdb/data/~2F"
-
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID")
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+
+
